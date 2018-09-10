24778e20bcc6f12fe010edfa88e5cebf5507bc86
trmatt <39041501+trmatt@users.noreply.github.com>
2018-09-09 19:42:45 -0500

    write test to confirm that URL is defined for allFeeds variable

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL are defined & not empty', function(){
+            for(let feed of allFeeds) {
+                expect(feed.url.length).toBeGreaterThan(0);
+            }
+        });
+
 
 
         /* TODO: Write a test that loops through each feed
